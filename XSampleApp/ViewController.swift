22454379fb0b17b917ef3afc0f96697b46caf124
SuzukiMasaya-ra1250s <ra1250s.ms@gmail.com>
2025-02-27 22:41:24 +0900

    プロジェクト作成

---
+++ XSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XSampleApp
+//
+//  Created by Masaya Suzuki on 2025/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
