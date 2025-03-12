5de91831fe23173b33c8cd9e36f26aff8c74aeba
SuzukiMasaya-ra1250s <ra1250s.ms@gmail.com>
2025-03-12 21:53:43 +0900

    .gitignoreにて監視対象外設定

--- Pods/Target Support Files/Pods-XSampleApp-XSampleAppUITests/Pods-XSampleApp-XSampleAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_XSampleApp_XSampleAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_XSampleApp_XSampleAppUITests
-@end
--- Pods/Target Support Files/Pods-XSampleApp/Pods-XSampleApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_XSampleApp : NSObject
-@end
-@implementation PodsDummy_Pods_XSampleApp
-@end
